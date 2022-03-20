24de6e769062440767a1bf3db4351faa9d816999
Dmitriy Sopp <d.sopp@groupstp.ru>
2022-03-20 16:06:22 +0800

    first comit

---
+++ js/tabs.js
@@ -0,0 +1,27 @@
+const tabButtons = document.querySelectorAll('.design-list__item')
+const tabDescriptions = document.querySelectorAll('.design__descr')
+const tabImages = document.querySelectorAll('.design-images')
+const tabFeature = document.querySelectorAll('.feature__img')
+
+tabButtons.forEach((tab) => {
+  tab.addEventListener('click', (event) => {
+    const dataValue = tab.dataset.tabsHandler
+    
+    document.title = tab.textContent
+
+    tabButtons.forEach((btn) =>  
+      btn === event.target 
+      ? btn.classList.add('design-list__item_active') 
+      : btn.classList.remove('design-list__item_active')
+    ) 
+
+    changeContent(tabImages, dataValue)
+    changeContent(tabFeature, dataValue)
+    changeContent(tabDescriptions, dataValue)
+  })
+})
+
+const changeContent = (array, value) =>  
+  array.forEach(elem => elem.dataset.tabsField === value ? elem.classList.remove('hidden') : elem.classList.add('hidden')) 
+
+
